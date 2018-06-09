f847415d05ec60f739513f4b7cefbfc119a0c613
Justin <justinpinkney@gmail.com>
2018-06-09 21:19:56 +0100

    html content test

--- tests/test_core.py
+++ tests/test_core.py
@@ -20,7 +20,7 @@ def test_load_notes(datadir):
     assert note.name == 'note_1'
     assert note.content == '# Note 1\n\ncontent\n'
 
-def test_html_renderer(datadir):
+def test_html_renderer_filename(datadir):
 
     note = note_builder.Note(name='note_1', content = '# Note 1\n\ncontent\n')
     expected_name = note.name + '.html'
@@ -30,3 +30,18 @@ def test_html_renderer(datadir):
     renderer.render(note)
 
     assert os.path.isfile(expected_filename)
+
+def test_html_content_no_style(datadir):
+
+    note_path = datadir.join('note_1.md')
+    note = note_builder.make_note(note_path)
+    expected_name = note.name + '.html'
+    expected_filename = datadir.join(expected_name)
+
+    renderer = note_builder.HtmlRenderer(output_directory=datadir)
+    renderer.render(note)
+
+    with open(expected_filename) as output_file:
+        with open(datadir.join('note_1_ref.html')) as reference_file:
+            for output, reference in zip(output_file, reference_file):
+                assert output == reference
