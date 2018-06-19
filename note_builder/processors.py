99b26a3629fb691ef3f0fa9bd5c4b2937f76b624
Justin <justinpinkney@gmail.com>
2018-06-19 22:18:05 +0100

    Quantifier processor

--- note_builder/processors.py
+++ note_builder/processors.py
@@ -1,5 +1,8 @@
 from collections import namedtuple
 import re
+from datetime import datetime
+
+import tinydb
 
 Measurement = namedtuple('Measurement', ['notes', 'lines', 'words'])
 
@@ -32,3 +35,23 @@ def measure_notes(notes):
         all_words += note_measurement.words
 
     return Measurement(all_notes, all_lines, all_words)
+
+class Quantifier(object):
+
+    def __init__(self, db_path):
+        self.db_path = db_path
+
+    def process(self, notes):
+        measurement = measure_notes(notes)
+        self.record(measurement)
+
+    def record(self, measurement):
+            db = tinydb.TinyDB(self.db_path)
+            now = datetime.now()
+            time_format = '%Y-%m-%d %H:%M:%S'
+            db.insert({
+                'time': now.strftime(time_format),
+                'notes': measurement.notes,
+                'lines': measurement.lines,
+                'words': measurement.words,
+            })
--- tests/test_processors.py
+++ tests/test_processors.py
@@ -1,5 +1,7 @@
 from .context import note_builder
 
+import os
+
 def test_measure():
 
     content = 'Note 1\nThe content\n'
@@ -9,7 +11,6 @@ def test_measure():
     assert measurements.notes == 1
     assert measurements.lines == 2
     assert measurements.words == 4
-
 
 def test_remove_special_characters():
 
@@ -33,3 +34,17 @@ def test_measure_notes():
     assert measurements.notes == 2
     assert measurements.lines == 4
     assert measurements.words == 8
+
+def test_quantifier(tmpdir):
+    note1 = note_builder.Note(name='note_1',
+                                content = '# Note 1\n\ncontent\n')
+    note2 = note_builder.Note(name='note_2',
+                                content = '# Second Note\nSome new content\n')
+    note_list = [note1, note2]
+    test_db = tmpdir.mkdir('db').join('test.db')
+    quantifier = note_builder.processors.Quantifier(test_db)
+
+    quantifier.process(note_list)
+
+    assert quantifier.db_path == test_db
+    assert os.path.isfile(quantifier.db_path)
