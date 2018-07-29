622d3709a32144eb6f681d16ffb0a8e256ded0e5
Justin <justinpinkney@gmail.com>
2018-07-29 22:24:10 +0100

    Testing click for cli

--- run.py
+++ run.py
@@ -2,34 +2,45 @@ import note_builder as nb
 
 import os
 import json
+import click
 
-with open('config.json', 'r') as read_file:
-    config = json.load(read_file)
+@click.command()
+@click.option('--stats/--no-stats', default=True)
+def build(stats):
+    with open('config.json', 'r') as read_file:
+        config = json.load(read_file)
 
-datadir = '.'
-assets = os.path.join(datadir, config['assets'])
-css_relative = config['css']
-css = [os.path.join(datadir, css_file) for css_file in css_relative]
-output_dir = config['output']
+    datadir = '.'
+    assets = os.path.join(datadir, config['assets'])
+    css_relative = config['css']
+    css = [os.path.join(datadir, css_file) for css_file in css_relative]
+    output_dir = config['output']
 
 
-note_files = nb.find_notes(datadir)
-notes = nb.load_notes(note_files)
-renderer = nb.HtmlRenderer(assets=assets, css=css)
-quantifier = nb.processors.Quantifier(os.path.join(datadir, 'stats_db'))
-tagger = nb.processors.TagIndex()
-gallery = nb.processors.Gallery()
+    note_files = nb.find_notes(datadir)
+    notes = nb.load_notes(note_files)
+    renderer = nb.HtmlRenderer(assets=assets, css=css)
+    quantifier = nb.processors.Quantifier(os.path.join(datadir, 'stats_db'))
+    tagger = nb.processors.TagIndex()
+    gallery = nb.processors.Gallery()
 
-builder = nb.Builder()
+    builder = nb.Builder()
 
-builder.add_processor(quantifier)
-builder.add_processor(tagger)
-builder.add_processor(gallery)
+    if stats:
+        builder.add_processor(quantifier)
 
-builder.add_renderer(quantifier)
-builder.add_renderer(tagger)
-builder.add_renderer(gallery)
-builder.add_renderer(renderer)
+    builder.add_processor(tagger)
+    builder.add_processor(gallery)
 
-notes = builder.process(notes)
-builder.render(output_dir, notes)
+    if stats:
+        builder.add_renderer(quantifier)
+
+    builder.add_renderer(tagger)
+    builder.add_renderer(gallery)
+    builder.add_renderer(renderer)
+
+    notes = builder.process(notes)
+    builder.render(output_dir, notes)
+
+if __name__ == '__main__':
+    build()
